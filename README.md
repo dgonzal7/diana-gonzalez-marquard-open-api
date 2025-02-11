fcefca1ab560c7d685db04137e76036bc09b3e76
Diana <180865641+dgonzal7@users.noreply.github.com>
2025-02-11 16:14:30 -0500

    updates to fetch API

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # diana-gonzalez-marquard-open-api
 Open Api Project for Intro to Programming course with Code the Dream
 
-[My Open API Project](https://github.com/dgonzal7/diana-gonzalez-marquard-open-api)
+This website is meant to provide a list of cat breeds for you to select from. Once you click on a cat breed from the drop down selector, you will see some cool details related to that cat breed along with a random photo of that particular breed.
+
+I used [The CAT API](thecatapi.com) to fetch this data. Particulary to filter by breed and receive details about that specific breed and photos.
+
+# Installation
+1. Download or clone the repository:
+
+git clone https://github.com/dgonzal7/diana-gonzalez-marquard-open-api.git
+
+2. Open the index.html file on your preferred web browser to run the website. 
 
 
--- index.js
+++ index.js
@@ -1,54 +1,75 @@
 let Allcatbreeds = []
 
 async function getBreed() {
-  const response = await fetch("https://api.thecatapi.com/v1/breeds")
-  const data = await response.json()
-  console.log("data: ", data);
+  try {  
+    const response = await fetch("https://api.thecatapi.com/v1/breeds");
+    // if response fails
+    if (!response.ok){
+      throw new Error(response.status);
+    }
+    // if successful do the below
+    const data = await response.json()
+    console.log("data: ", data);
   
-  //all breeds and details are in one Array
-  //console.log(Array.isArray(data));
+    //all breeds and details are in one Array
+    //console.log(Array.isArray(data));
 
-  const breedSection = document.getElementById("breed");
+    const breedSection = document.getElementById("breed");
 
-  Allcatbreeds = data;
+    Allcatbreeds = data;
 
-  //loop through the list and grab each breed)
-  for (let i = 0; i < data.length; i++) {
-    const eachBreed = Allcatbreeds[i];
-    //console.log(eachBreed.name);
-    //console.log(data);
-    //console.log(eachBreed.id);
+    //loop through the arry and grab each breed)
+    for (let i = 0; i < data.length; i++) {
+      const eachBreed = Allcatbreeds[i];
 
-    //DOM selectors - get the div, then get the select element, then create the options list
-    const catSection = document.getElementById("breed");
-    const selectOption = catSection.querySelector("select");
-    const catOption = document.createElement("option");
+      //DOM selectors - get the div, then get the select element, then create the options list
+      const catSection = document.getElementById("breed-selectdiv");
+      const selectOption = catSection.querySelector("select");
+      const catOption = document.createElement("option");
 
-    //add each breed name as an option to the list
-    catOption.value = eachBreed.id;
-    catOption.innerHTML = `${eachBreed.name}`;
-    selectOption.appendChild(catOption);
+      //add each breed name as an option to the list
+      catOption.value = eachBreed.id;
+      catOption.innerHTML = `${eachBreed.name}`;
+      selectOption.appendChild(catOption);
+    } 
+  } catch (error) {
+  console.error(error);
   }
 }
 
 getBreed();
 
 async function getCatDetails(x) {
-  //console.log("Selected x breed:", x)
-  const response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${x}&api_key=live_U53C7RuoG4NdjpCk3wlbWPkWE8drqRGbUe9QSXrPLlgOA1ZY2ic5RO5tUIMs6Gef`)
-  //console.log("response:", response);
-  const data = await response.json()
-  console.log("All data:", data);
-  //console.log("data url:", data[0].url);
+  try {
+    const response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${x}&api_key=live_U53C7RuoG4NdjpCk3wlbWPkWE8drqRGbUe9QSXrPLlgOA1ZY2ic5RO5tUIMs6Gef`)
+  //if response fails
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+    //if fetch is successful do the below 
+    const data = await response.json();
 
-  const cat_image = document.querySelector("#cat-image");
-  //console.log("Cat image:", cat_image);
+    const cat_image = document.querySelector("#cat-image");
+    const descriptionElement = document.getElementById("description"); 
+    const temperamentElement = document.getElementById("temperament"); 
+    const originElement = document.getElementById("origin");
+    const wiki_link = document.getElementById("wiki-link");
 
-  cat_image.classList.remove('hidden');
-  cat_image.src = data[0].url;
+    cat_image.classList.remove('hidden');
+    cat_image.src = data[0].url;
 
-  document.getElementById("description").textContent = `Description: ${data[0].breeds[0].description}`;
-  document.getElementById("temperament").textContent = `Temperament: ${data[0].breeds[0].temperament}`;
-  document.getElementById("origin").textContent = `Country of Origin: ${data[0].breeds[0].origin}`;
-  
+    //bring in description, temperament, and country of origin and add text content
+    descriptionElement.textContent = `Description: ${data[0].breeds[0].description}`;
+    temperamentElement.textContent = `Temperament: ${data[0].breeds[0].temperament}`;
+    originElement.textContent = `Country of Origin: ${data[0].breeds[0].origin}`;
+    
+    wiki_link.classList.remove('hidden');
+    wiki_link.href = data[0].breeds[0].wikipedia_url;
+
+
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+
